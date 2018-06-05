3c61e6333dd5654f7539a6d9cbde9d735ac89c3f
Joe Gray <jgray@advancedpersistentsecurity.net>
2018-06-04 21:03:23 -0400

    Create passwordcode.py

---
+++ passwordcode.py
@@ -0,0 +1,60 @@
+print "Welcome to the Password Code Compliance Checking Tool."
+print "This is a satire tool written by @C_3PJoe to complement"
+print "his State Password Inspector badge made by @SkyDog"
+print " "
+print " "
+print " "
+
+def main():
+    passw = raw_input("Please Enter Your Password.")
+    if len(passw) > 0:
+        print "This password is compliant!"
+        anth = raw_input("Would you like to help with my anthropology survey? \t (y/n)")
+        if anth == "y":
+            anthr()
+        else:
+            exit()
+    else:
+        exit()
+
+def anthr():
+    mmn = raw_input("What was your mom's name before she was married?")
+    if len(mmn) < 1:
+        exit()
+    else:
+       print "QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
+       print "QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
+       print "QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
+       print "QQQQQQQQQQQQQQQQQQQWQQQQQWWWBBBHHHHHHHHHBWWWQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQ"
+       print "QQQQQQQQQQQQQQQD!`__ssaaaaaaaaaass_ass_s____.  -~''??9VWQQQQQQQQQQQQQQQQQQQ"
+       print "QQQQQQQQQQQQQP'_wmQQQWWBWV?GwwwmmWQmwwwwwgmZUVVHAqwaaaac,'?9$QQQQQQQQQQQQQQ"
+       print "QQQQQQQQQQQW! aQWQQQQW?qw#TTSgwawwggywawwpY?T?TYTYTXmwwgZ$ma/-?4QQQQQQQQQQQ"
+       print "QQQQQQQQQQW' jQQQQWTqwDYauT9mmwwawww?WWWWQQQQQ@TT?TVTT9HQQQQQQw,-4QQQQQQQQQ"
+       print "QQQQQQQQQQ[ jQQQQQyWVw2$wWWQQQWWQWWWW7WQQQQQQQQPWWQQQWQQw7WQQQWWc)WWQQQQQQQ"
+       print "QQQQQQQQQf jQQQQQWWmWmmQWU???????9WWQmWQQQQQQQWjWQQQQQQQWQmQQQQWL 4QQQQQQQQ"
+       print "QQQQQQQP'.yQQQQQQQQQQQP'       <wa,.!4WQQQQQQQWdWP??!'??4WWQQQWQQc ?QWQQQQQ"
+       print "QQQQQP'_a.<aamQQQW!<yF '!` ..  '??$Qa 'WQQQWTVP'    '??' =QQmWWV?46/ ?QQQQQ"
+       print "QQQP'sdyWQP?!`.-'?46mQQQQQQT!mQQgaa. <wWQQWQaa _aawmWWQQQQQQQQQWP4a7g -WWQQ"
+       print "QQ[ j@mQP'adQQP4ga, -????' <jQQQQQWQQQQQQQQQWW;)WQWWWW9QQP?'`  -?QzQ7L ]QQQ"
+       print "QW jQkQ@ jWQQD'-?$QQQQQQQQQQQQQQQQQWWQWQQQWQQQc '4QQQQa   .QP4QQQQfWkl jQQQ"
+       print "QE ]QkQk $D?`  waa '?9WWQQQP??T?47`_aamQQQQQQWWQw,-?QWWQQQQQ`'QQQD\Qf(.QWQQ"
+       print "QQ,-Qm4Q/-QmQ6 'WWQma/  '??QQQQQQL 4W'- -?$QQQQWP`s,awT$QQQ@  'QW@?$:.yQQQQ"
+       print "QQm/-4wTQgQWQQ,  ?4WWk 4waac -???$waQQQQQQQQF??'<mWWWWWQW?^  ` ]6QQ' yQQQQQ"
+       print "QQQQw,-?QmWQQQQw  a,    ?QWWQQQw _.  '????9VWaamQWV???'  a j/  ]QQf jQQQQQQ"
+       print "QQQQQQw,'4QQQQQQm,-$Qa     ???4F jQQQQQwc <aaas _aaaaa 4QW ]E  )WQ`=QQQQQQQ"
+       print "QQQQQQWQ/ $QQQQQQQa ?H ]Wwa,     ???9WWWh dQWWW,=QWWU?  ?!     )WQ ]QQQQQQQ"
+       print "QQQQQQQQQc-QWQQQQQW6,  QWQWQQQk <c                             jWQ ]QQQQQQQ"
+       print "QQQQQQQQQQ,'$WQQWQQQQg,.'?QQQQ'.mQQQmaa,.,                . .; QWQ.]QQQQQQQ"
+       print "QQQQQQQQQWQa ?$WQQWQQQQQa,.'?( mQQQQQQW[:QQQQm[ ammF jy! j( } jQQQ(:QQQQQQQ"
+       print "QQQQQQQQQQWWma '9gw?9gdB?QQwa, -??T$WQQ;:QQQWQ ]WWD _Qf +?! _jQQQWf QQQQQQQ"
+       print "QQQQQQQQQQQQQQQws 'Tqau?9maZ?WQmaas,,    --~-- ---  . _ssawmQQQQQQk 3QQQQWQ"
+       print "QQQQQQQQQQQQQQQQWQga,-?9mwad?1wdT9WQQQQQWVVTTYY?YTVWQQQQWWD5mQQPQQQ ]QQQQQQ"
+       print "QQQQQQQWQQQQQQQQQQQWQQwa,-??$QwadV}<wBHHVHWWBHHUWWBVTTTV5awBQQD6QQQ ]QQQQQQ"
+       print "QQQQQQQQQQQQQQQQQQQQQQWWQQga,-'9$WQQmmwwmBUUHTTVWBWQQQQWVT?96aQWQQQ ]QQQQQQ"
+       print "QQQQQQQQQQWQQQQWQQQQQQQQQQQWQQma,-?9$QQWWQQQQQQQWmQmmmmmQWQQQQWQQW(.yQQQQQW"
+       print "QQQQQQQQQQQQQWQQQQQQWQQQQQQQQQQQQQga%,.  -??9$QQQQQQQQQQQWQQWQQV? sWQQQQQQQ"
+       print "QQQQQQQQQWQQQQQQQQQQQQQQWQQQQQQQQQQQWQQQQmywaa,;~^'!???????!^`_saQWWQQQQQQQ"
+       print "QQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQQWWWWQQQQQmwywwwwwwmQQWQQQQQQQQQQQ"
+       print "QQQQQQQWQQQWQQQQQQWQQQWQQQQQWQQQQQQQQQQQQQQQQWQQQQQWQQQWWWQQQQQQQQQQQQQQQWQ"
+
+main()
